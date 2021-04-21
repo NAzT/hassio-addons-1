fc0051aa6008d4498f6527fb00e0b300eab1a56c
Nat W <nat.wrw@gmail.com>
2021-04-21 09:11:16 +0700

    use config

--- myapp/run.sh
+++ myapp/run.sh
@@ -7,55 +7,31 @@ bashio::log.info "Public key:"
 
 cat /root/.ssh/id_rsa.pub
 
-BASE="ssh -o StrictHostKeyChecking=no"
-TUN="-o ExitOnForwardFailure=yes -o ServerAliveInterval=30 -N"
+# BASE="ssh -o StrictHostKeyChecking=no"
+# TUN="-o ExitOnForwardFailure=yes -o ServerAliveInterval=30 -N"
 
-if bashio::config.true 'tunnel_http'; then
-    # -R remote_socket:host:hostport
-    # Specifies that connections to the given TCP port or Unix socket on the
-    # remote (server) host are to be forwarded to the local side.
-    TUN="${TUN} -R 80:localhost:80"
+configPath="/etc/cloudflared/config.yml"
+mkdir -p /etc/cloudflared
+
+echo "log: stdout" > $configPath
+bashio::log.debug "Hello this is debug log from bashio!"
+# if bashio::var.has_value "$(bashio::addon.port 4040)"; then
+#   echo "web_addr: 0.0.0.0:$(bashio::addon.port 4040)" >> $configPath
+# fi
+if bashio::var.has_value "$(bashio::config 'host')"; then
+  echo "host: $(bashio::config 'host')" >> $configPath
 fi
 
-if bashio::config.true 'tunnel_https'; then
-    TUN="${TUN} -R 443:localhost:443"
-fi
+if bashio::var.has_value "$(bashio::config 'url')"; then
+  echo "url: $(bashio::config 'url')" >> $configPath
 
-if ! bashio::config.equals 'socks_port' 0; then
-    # -D [bind_address:]port
-    # Currently the SOCKS4 and SOCKS5 protocols are supported, and ssh will act
-    # as a SOCKS server.
-    TUN="${TUN} -D *:$(bashio::config 'socks_port')"
-fi
-
-if ! bashio::config.is_empty 'advanced'; then
-    TUN="${TUN} $(bashio::config 'advanced')"
-fi
-
-SRV="$(bashio::config 'ssh_user')@$(bashio::config 'ssh_host')"
-
-if ! bashio::config.equals 'ssh_port' 22; then
-    SRV="-p $(bashio::config 'ssh_port') ${SRV}"
-fi
-
-SRV="-o StrictHostKeyChecking=no ${SRV}"
-
-# set +e
-
-# while true
-# do
-#     if ! bashio::config.is_empty 'before'; then
-#         CMD="${BASE} ${SRV} $(bashio::config 'before')"
-#         bashio::log.info "[ $(date +'%m-%d-%Y') ] run: ${CMD}"
-#         eval $CMD
-#     fi
-
-#     CMD="${BASE} ${TUN} ${SRV}"
-#     bashio::log.info "[ $(date +'%m-%d-%Y') ] run tunnel: ${CMD}"
-#     eval $CMD
-
-#     sleep 30
-# done
-
-
+# if bashio::var.has_value "$(bashio::config 'auth_token')"; then
+#   echo "authtoken: $(bashio::config 'auth_token')" >> $configPath
+# fi
+# if bashio::var.has_value "$(bashio::config 'region')"; then
+#   echo "region: $(bashio::config 'region')" >> $configPath
+# else
+#   echo "No region defined, default region is US."
+# fi
+cat $configPath
 cloudflared --url localhost:8123
